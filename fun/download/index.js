3786198fe1e2f2031a48a5ac8c43a14d7eb291c8
smartBBer <bai5775@outlook.com>
2019-03-27 19:44:22 +0800

    download

---
+++ fun/download/index.js
@@ -0,0 +1,90 @@
+// download/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    loadingHidden:true
+  },
+
+  load: function() {
+    this.setData({
+      loadingHidden: false
+    })  
+    let _that = this;
+    wx.downloadFile({
+      url: 'https://lg-332ncmjq-1256907309.cos.ap-shanghai.myqcloud.com/stepking/a.xlsx',
+      success: function(res) {
+        var filePath = res.tempFilePath;
+        console.log(res)     //页面显示加载动画        
+        wx.openDocument({     
+          filePath: filePath,
+               success: function(res) {      
+            _that.setData({       
+              loadingHidden: true      
+            })      
+            console.log('打开文档成功')     
+          }    
+        })   
+      }  
+    }) 
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
